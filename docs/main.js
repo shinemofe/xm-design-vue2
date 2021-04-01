467b483f582c5d0986a7da1c487c67cd63201ee5
yangming <yangming@shinemo.com>
2021-04-01 17:15:42 +0800

    fix theme

--- docs/main.js
+++ docs/main.js
@@ -11,14 +11,14 @@ tconModules.forEach(x => {
   require(`tcon/dist/${x}.css`)
 })
 
-// 变量预览时覆盖
-const css = localStorage.getItem('xm-design-vue2-theme')
-if (css) {
-  const style = document.createElement('style')
-  style.innerHTML = css
-  style.setAttribute('type', 'text/css')
-  document.head.appendChild(style)
-}
+// 变量预览时覆盖由子项目决定
+// const css = localStorage.getItem('xm-design-vue2-theme')
+// if (css) {
+//   const style = document.createElement('style')
+//   style.innerHTML = css
+//   style.setAttribute('type', 'text/css')
+//   document.head.appendChild(style)
+// }
 
 // <demo-code /> 可以在 md 中插入组件或者组件源码
 Vue.component('demo-playground', {
--- examples/main.js
+++ examples/main.js
@@ -4,18 +4,32 @@ import router from './router'
 import './vant.copy.css'
 import { tconModules } from '../doc.config'
 import '../src/style/index.less'
+import Dialog from 'vant/es/dialog'
+import 'vant/es/dialog/index.less'
+import 'vant/es/overlay/index.less'
 
 tconModules.forEach(x => {
   require(`tcon/dist/${x}.css`)
 })
 
 // 变量预览时覆盖
+const isParentPreview = /preview/.test(window.top.location.search)
 const css = localStorage.getItem('xm-design-vue2-theme')
 if (css) {
-  const style = document.createElement('style')
-  style.innerHTML = css
-  style.setAttribute('type', 'text/css')
-  document.head.appendChild(style)
+  const insert = () => {
+    const style = document.createElement('style')
+    style.innerHTML = css
+    style.setAttribute('type', 'text/css')
+    document.head.appendChild(style)
+    window.top.document.head.appendChild(style.cloneNode())
+  }
+  if (!isParentPreview) {
+    Dialog.confirm({ message: '本地主题存在，是否加载本地主题样式' }).then(() => {
+      insert()
+    }).catch(() => {
+      localStorage.removeItem('xm-design-vue2-theme')
+    })
+  }
 }
 
 const app = new Vue({
